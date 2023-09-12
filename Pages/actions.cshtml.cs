d231a294c183a9899249c62ffb16cb4ca14c1cf6
Karnnapat <karnptn123@gmail.com>
2023-09-12 11:13:27 +0700

    first commit

---
+++ Pages/actions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nayai_Restaurant_Project.Pages
+{
+    public class actionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/ui.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nayai_Restaurant_Project.Pages
+{
+    public class uiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
